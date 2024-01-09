922f4707667887cf1a8def184ae1b85a1684599a
tmuco <mucotresor@gmail.com>
2024-01-09 12:23:13 +0200

    feat(app): support greeting people in Spanish

--- hello.go
+++ hello.go
@@ -3,15 +3,20 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name, lang string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if lang == "Spanish" {
+		return spanishHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -4,17 +4,22 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Tresor")
+		got := Hello("Tresor", "")
 		want := "Hello, Tresor"
 		assertEqual(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 		assertEqual(t, got, want)
 	})
 
+	t.Run("saying hello to people in Spanish", func(t *testing.T) {
+		got := Hello("Tresor", "Spanish")
+		want := "Hola, Tresor"
+		assertEqual(t, got, want)
+	})
 }
 
 func assertEqual(t testing.TB, got, want string) {
