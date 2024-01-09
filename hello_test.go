15f15f6997c45961aacf5a60dc3792560c5a6263
tmuco <mucotresor@gmail.com>
2024-01-09 12:11:31 +0200

    refactor(tools): add an assertEqual test util

--- hello_test.go
+++ hello_test.go
@@ -6,19 +6,20 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Tresor")
 		want := "Hello, Tresor"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertEqual(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertEqual(t, got, want)
 	})
 
+}
+
+func assertEqual(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
